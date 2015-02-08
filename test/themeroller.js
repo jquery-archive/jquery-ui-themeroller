f124f0eb1cab15a5f63d2a13bb6feaf18d9b69bf
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-02-08 11:31:49 -0200

    Fix images (icons and textures) path

--- test/themeroller.js
+++ test/themeroller.js
@@ -3,16 +3,31 @@ var expect = require( "chai" ).expect,
 	ThemeRoller = require( "../lib/themeroller.js" );
 
 describe( "ThemeRoller", function() {
+	var theme;
 
-	it( "should build just fine", function() {
-		var theme,
-			baseThemeCss = fs.readFileSync( __dirname + "/fixtures/jquery-ui-1.12/base/theme.css" ),
+	it( "should instantiate", function() {
+		var baseThemeCss = fs.readFileSync( __dirname + "/fixtures/jquery-ui-1.12/base/theme.css" ),
 			vars = require( "./fixtures/vars/base.json" );
 
 		theme = new ThemeRoller( baseThemeCss, vars );
+		expect( theme ).to.be.an.instanceof( ThemeRoller );
+	});
+
+	it( "should generate the theme CSS", function() {
 		expect( theme.css() ).to.equal(
 			fs.readFileSync( __dirname + "/fixtures/jquery-ui-1.12/themes/base.css" ).toString( "utf-8" )
 		);
+	});
+
+	it( "should generate images", function( done ) {
+		theme.generateImages(function( error, images ) {
+			try {
+				expect( error ).to.be.null;
+				expect( images ).to.be.an( "object" );
+			} finally {
+				done();
+			}
+		});
 	});
 
 });
